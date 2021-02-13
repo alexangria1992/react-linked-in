52e398ca6f917d498c81c1cb3e9e595fe5f4a485
alexangria1992 <alex040892@gmail.com>
2021-02-14 00:02:53 +1300

    1st part of Sidebar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-
+import Sidebar from './Sidebar'
 function App() {
   return (
     <div className="App">
-
-      {/* Header */}
       <Header/>
-      {/* App Body  */}
 
-      {/* Sidebar  */}
+      {/* App Body  */}
+      <div className="app__body">
+        <Sidebar/>
+      </div>
 
       {/* Feed */}
 
