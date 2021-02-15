84a0dcf139ddfb5c8562569bcbd2582fc9d78217
alexangria1992 <alex040892@gmail.com>
2021-02-16 00:53:01 +1300

    Input Options (Feed)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Feed from './Feed';
 import Header from './Header';
-import InputOption from './InputOption';
 import Sidebar from './Sidebar'
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
       <div className="app__body">
         <Sidebar/>
         <Feed/>
-        <InputOption/>
       </div>
 
       {/* Feed */}
--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import './Feed.css'
 import CreateIcon from "@material-ui/icons/Create"
+import ImageIcon from "@material-ui/icons/Image"
+import EventNoteIcon from "@material-ui/icons/EventNote"
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay"
+
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions"
+
+import InputOption from './InputOption'
+
 
 function Feed() {
     return (
@@ -13,10 +21,15 @@ function Feed() {
                         <button type='submit'>Send</button>
                     </form>
                 </div>
-                <div className="feed_InputOptions">
-                    <InputOption/>
+                <div className="feed_inputOptions">
+                    <InputOption Icon={ImageIcon} title='photo' color="#70B5F9"/>
+                    <InputOption Icon={SubscriptionsIcon} title='video' color="#E7A33E"/>
+                    <InputOption Icon={EventNoteIcon} title='Event' color="#C0CBCD"/>
+                    <InputOption Icon={CalendarViewDayIcon} title='Write Article' color="#7FC15E"/>
+
                 </div>
             </div>
+            {/* Posts */}
         </div>
     )
 }
